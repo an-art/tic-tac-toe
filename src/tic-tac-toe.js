9bf330602b27aee6e05fecaf4aace152f6267489
an-art <anartyomchik@gmail.com>
2017-02-05 00:29:48 +0300

    getCurrentPlayerSymbol, nextTurn, noMoreTurns, getFieldValue

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,28 @@
 class TicTacToe {
     constructor() {
-
+        this.symbol = 'x';
+        this.a = [new Array(3),
+                new Array(3),
+                new Array(3)];
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.symbol;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (!this.a[rowIndex][columnIndex]){
+        this.a[rowIndex][columnIndex] = this.symbol;
+        switch (this.symbol) {
+            case 'x':
+            this.symbol = 'o';
+            break;
+            case 'o':
+            this.symbol = 'x';
+            break;
+        }} else {
+            return this.symbol;
+        }
     }
 
     isFinished() {
@@ -20,7 +34,11 @@ class TicTacToe {
     }
 
     noMoreTurns() {
-
+        for (var i=0;i<3;i++) {
+                for (var j=0;j<3;j++) {
+                    if (!this.a[i][j]) return false; 
+                }
+            } return true;
     }
 
     isDraw() {
@@ -28,7 +46,8 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        if (this.a[rowIndex][colIndex]) return this.a[rowIndex][colIndex];
+        else return null;
     }
 }
 
