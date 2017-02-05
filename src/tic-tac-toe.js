b239e7faf0c57c6b7a3be881596697827b22bba5
an-art <anartyomchik@gmail.com>
2017-02-05 21:07:42 +0300

    done

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,6 +1,7 @@
 class TicTacToe {
     constructor() {
         this.symbol = 'x';
+        this.prev;
         this.a = [new Array(3),
                 new Array(3),
                 new Array(3)];
@@ -13,6 +14,7 @@ class TicTacToe {
     nextTurn(rowIndex, columnIndex) {
         if (!this.a[rowIndex][columnIndex]){
         this.a[rowIndex][columnIndex] = this.symbol;
+        this.prev = this.symbol;
         switch (this.symbol) {
             case 'x':
             this.symbol = 'o';
@@ -26,11 +28,36 @@ class TicTacToe {
     }
 
     isFinished() {
-
+        if (this.getWinner()!==null || this.noMoreTurns() === true) return true;
+        else return false;
     }
 
     getWinner() {
-
+        var winner = "";
+        //Check across the row
+        for (var i = 0; i < 3; ++i) {
+            if (this.a[i][0] !== "" && this.a[i][0] === this.a[i][1] && this.a[i][1]===this.a[i][2]) {
+                winner = this.a[i][0];
+            }
+        }
+        //Check down the columns
+        if(!winner){
+            for (var i = 0; i < 3; ++i) {
+                if (this.a[0][i]!=="" && this.a[0][i]===this.a[1][i] && this.a[1][i]===this.a[2][i]) {
+                    winner = this.a[0][i];
+                } 
+            }
+        }
+        //Check on the diagonals
+        if(!winner){
+            if (this.a[0][0]!=="" && this.a[0][0]===this.a[1][1] && this.a[1][1]===this.a[2][2]) {
+                winner = this.a[0][0];
+            } else if (this.a[0][2]!=="" && this.a[0][2]===this.a[1][1] && this.a[1][1]===this.a[2][0]) {
+                winner = this.a[0][2];
+            }
+        }
+        if (!winner) return null;
+        else return this.prev;
     }
 
     noMoreTurns() {
@@ -42,7 +69,9 @@ class TicTacToe {
     }
 
     isDraw() {
-
+        if (this.isFinished === false || this.getWinner()!==null ) return false;
+        else  if (this.noMoreTurns() === true) return true;
+        else return false;
     }
 
     getFieldValue(rowIndex, colIndex) {
